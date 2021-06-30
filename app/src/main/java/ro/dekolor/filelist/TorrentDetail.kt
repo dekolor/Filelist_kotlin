98d71df6c45e7758da3d919739c1bd3ba3bfd28b
stefan <stefanbriceag1@gmail.com>
2021-06-30 19:20:17 +0300

    torrentdetail update

--- app/src/main/java/ro/dekolor/filelist/TorrentDetail.kt
+++ app/src/main/java/ro/dekolor/filelist/TorrentDetail.kt
@@ -2,8 +2,10 @@ package ro.dekolor.filelist
 
 import android.content.Context
 import android.content.SharedPreferences
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_torrent_detail.*
 import ro.dekolor.filelist.models.Torrent
@@ -22,17 +24,33 @@ class TorrentDetail : AppCompatActivity() {
                 var torrenteJson = mSharedPreferences.getString(Constants.LATEST_TORRENT_DATA, "")
                 var torrentedata = Gson().fromJson(torrenteJson, Array<Torrent>::class.java).toList() as ArrayList<Torrent>
                 val torrentid = intent.getStringExtra("torrent_id").toString()
-                id_torrent.setText(torrentid)
                 var torrent = torrentedata.filter { it.id.toString() == torrentid}.single()
-                title_torrent.text = torrent.name.toString()
+                torrent_name.text = torrent.name.toString()
+                if(torrent.freeleech==1) {
+                    card_freeleech.visibility = View.VISIBLE
+                }
+                if(torrent.internal==1) {
+                    card_internal.visibility = View.VISIBLE
+                }
+                if(torrent.doubleup==1) {
+                    card_doubleup.visibility = View.VISIBLE
+                }
             }
             else if(intent.hasExtra("type") && intent.getStringExtra("type") == "search") {
                 var torrenteJson = mSharedPreferences.getString(Constants.SEARCH_TORRENT_DATA, "")
                 var torrentedata = Gson().fromJson(torrenteJson, Array<Torrent>::class.java).toList() as ArrayList<Torrent>
                 val torrentid = intent.getStringExtra("torrent_id").toString()
-                id_torrent.setText(torrentid)
                 var torrent = torrentedata.filter { it.id.toString() == torrentid}.single()
-                title_torrent.text = torrent.name.toString()
+                torrent_name.text = torrent.name.toString()
+                if(torrent.freeleech==1) {
+                    card_freeleech.visibility = View.VISIBLE
+                }
+                if(torrent.internal==1) {
+                    card_internal.visibility = View.VISIBLE
+                }
+                if(torrent.doubleup==1) {
+                    card_doubleup.visibility = View.VISIBLE
+                }
             }
         }
     }
